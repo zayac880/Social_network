1cb475dd1897423dda451b58e3a949e42518c53e
Alex <zayac880880@mail.ru>
2023-01-10 04:55:12 +0300

    Initial commit

---
+++ app.py
@@ -0,0 +1,48 @@
+from flask import Flask, request, render_template
+from utils import get_posts_all, get_comments_by_post_id, get_post_by_pk, search_for_posts, get_posts_by_user
+
+
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def home():
+   items = get_posts_all()
+   return render_template("index.html", items=items)
+
+
+@app.route("/search")
+def search():
+   query = request.args.get("s")
+   posts = search_for_posts(query)
+   count_posts = len(posts)
+   return render_template("search.html", posts=posts, count_posts=count_posts)
+
+
+@app.route("/posts/<postid>")
+def viewing(postid):
+   comments = get_comments_by_post_id(postid)
+   item = get_post_by_pk(postid)
+   return render_template("post.html", item=item, comments=comments)
+
+
+@app.route("/user-feed/<username>")
+def post_user(username):
+   user = get_posts_by_user(username)
+   return render_template("user-feed.html", user=user)
+
+
+
+@app.errorhandler(404)
+def page_not_found(error):
+    return render_template('404.html'), 404
+
+
+@app.errorhandler(500)
+def page_server_error(error):
+    return render_template('500.html'), 500
+
+
+if __name__ == "__main__":
+    app.run(debug=True, port=5001)
---
+++ utils.py
@@ -0,0 +1,58 @@
+import json
+import os
+
+def get_posts_all():
+    """Функция загружает посты из файла"""
+    with open("C:/Users/Alex/Desktop/Sky_python/kursovaya_inst/data/posts.json", "r", encoding="utf-8") as f:
+        posts = json.load(f)
+        return posts
+
+
+def get_posts_by_user(user_name):
+    """Функция возвращает посты определенного пользователя."""
+    post = []
+    for user in get_posts_all():
+        if user["poster_name"] in user_name:
+            post.append(user)
+    if not post:
+        return ValueError
+    else:
+        return post
+
+
+def get_comments_by_post_id(post_id):
+    """Функция возвращает комментарии определенного поста."""
+    with open("C:/Users/Alex/Desktop/Sky_python/kursovaya_inst/data/comments.json", "r", encoding="utf-8") as f:
+        comment = []
+        comments = json.load(f)
+        for comment_one in comments:
+            if comment_one["post_id"] == int(post_id):
+                comment.append(comment_one)
+        if not comment:
+            return ValueError
+        else:
+            return comment
+
+
+def search_for_posts(query):
+    """Функция возвращает список постов по ключевому слову."""
+    posts_for_query = []
+    for post in get_posts_all():
+        if str(query) in post["content"]:
+            posts_for_query.append(post)
+    return posts_for_query
+
+
+def get_post_by_pk(pk):
+    """Функция возвращает один пост по его pk."""
+    number = []
+    number.append(int(pk))
+    for post in get_posts_all():
+        if post["pk"] in number:
+            return post
+
+
+def get_posts():
+    """Ф"""
+    data = get_posts_all()
+    return data
