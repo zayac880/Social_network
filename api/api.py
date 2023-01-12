9aa60ca18025ac91755aff14faa17dec49ec267f
zayac880 <119949619+zayac880@users.noreply.github.com>
2023-01-12 15:55:45 +0300

    Update api.py

--- api/api.py
+++ api/api.py
@@ -1,25 +1,17 @@
-from flask import Flask, jsonify
+from flask import Blueprint, jsonify
 from utils import get_posts, get_post_by_pk
-import logging
 
 
-logging.basicConfig(level=logging.INFO, filename="../logs/py_log.log", filemode="w",
-                    format="%(asctime)s [%(levelname)s] %(message)s")
 
-app = Flask(__name__)
-app.config["JSON_AS_ASCII"] = False
+api_blueprint = Blueprint('api_blueprint', __name__, template_folder='templates')
 
-
-@app.route('/posts', methods=['GET'])
+@api_blueprint.route('/api/posts', methods=['GET'])
 def load_posts():
     posts = get_posts()
     return jsonify(posts)
 
-@app.route('/posts/<post_id>', methods=['GET'])
+
+@api_blueprint.route('/api/posts/<post_id>', methods=['GET'])
 def read_post(post_id):
     post = get_post_by_pk(post_id)
     return jsonify(post)
-
-
-if __name__ == "__main__":
-    app.run(debug=True, port=5009)
